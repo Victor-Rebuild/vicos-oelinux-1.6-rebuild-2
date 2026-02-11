dfcd010eef26585cf09774f6ebe98191ae895613
Switch-modder <deolrajjyot@gmail.com>
2026-02-10 19:01:23 -0500

    Force no docker

--- build/build-and-send-to-modder-my-to.sh
+++ build/build-and-send-to-modder-my-to.sh
@@ -142,17 +142,17 @@ echo "Starting build"
 export AUTO_UPDATE=1
 
 echo "Dev"
-time ./build/build.sh -bt dev -s -op $OTA_PASS -v $VERSION_CODE -ir $BUILD_STACK -au
+time ./build/build.sh -bt dev -s -op $OTA_PASS -v $VERSION_CODE -ir $BUILD_STACK -au -nd
 scp -P 44 -i ~/modder-my-key _build/*.ota raj-jyot@modder.my.to:/media/raj-jyot/modder-my-to/webserver/otas/1.6-rebuild/$BUILD_STACK/dev/
 scp -P 44 -i ~/modder-my-key _build/*.ota raj-jyot@modder.my.to:/media/raj-jyot/modder-my-to/webserver/otas/1.6-rebuild/$BUILD_STACK/dev/latest
 
 echo "OSKR"
-time ./build/build.sh -bt oskr -s -op $OTA_PASS -bp $oskr_boot_password -v $VERSION_CODE -ir $BUILD_STACK -au
+time ./build/build.sh -bt oskr -s -op $OTA_PASS -bp $oskr_boot_password -v $VERSION_CODE -ir $BUILD_STACK -au -nd
 scp -P 44 -i ~/modder-my-key _build/*.ota raj-jyot@modder.my.to:/media/raj-jyot/modder-my-to/webserver/otas/1.6-rebuild/$BUILD_STACK/oskr/
 scp -P 44 -i ~/modder-my-key _build/*.ota raj-jyot@modder.my.to:/media/raj-jyot/modder-my-to/webserver/otas/1.6-rebuild/$BUILD_STACK/oskr/latest
 
 echo "Prod"
-time ./build/build.sh -bt proddev -s -op $OTA_PASS -bp $prod_boot_password -v $VERSION_CODE -ir $BUILD_STACK -au
+time ./build/build.sh -bt proddev -s -op $OTA_PASS -bp $prod_boot_password -v $VERSION_CODE -ir $BUILD_STACK -au -nd
 scp -P 44 -i ~/modder-my-key _build/*.ota raj-jyot@modder.my.to:/media/raj-jyot/modder-my-to/webserver/otas/1.6-rebuild/$BUILD_STACK/prod/
 scp -P 44 -i ~/modder-my-key _build/*.ota raj-jyot@modder.my.to:/media/raj-jyot/modder-my-to/webserver/otas/1.6-rebuild/$BUILD_STACK/prod/latest
 
